9d984edceb2e7d8a49229e7ac44ee3d6d00640fb
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-04-14 16:39:15 +0200

    quitando condfiguracion whatwg-fetch

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+//instalacion de pollyfill para ejecutar fetch en versiones anteriores a node 18:
+//instalamos whatwg-fetch (yarn add -D whatwg-fetch)
+//a continuacion importamos el paquete
+// import 'whatwg-fetch'
+
